d16e83bd94b8111be242f0d3c7a642a3fdda4ece
Ethan Smith <EthanKSmith@gmail.com>
2023-01-11 12:53:03 -0700

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,40 @@
+import React, { Component } from "react";
+
+export class App extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Hello Dojo!</h1>
+        <h2>Thing I need to do:</h2>
+        <ul>
+          <li>Learn React</li>
+          <li>Climb Mt. Everest</li>
+          <li>Run a marathon</li>
+          <li>Feed the dogs</li>
+        </ul>
+      </div>
+    );
+  }
+}
+
+export default App;
+
+// import logo from './logo.svg';
+// import './App.css';
+
+// function App() {
+//   return (
+//     <div className="App">
+//       <h1>Hello Dojo!</h1>
+//       <h2>Things I need to do:</h2>
+//       <ul>
+//         <li>Learn React</li>
+//         <li>Climb Mt. Everest</li>
+//         <li>Run a marathon</li>
+//         <li>Feed the dogs</li>
+//       </ul>
+//     </div>
+//   );
+// }
+
+// export default App;
